22a04bb9355061c45c8ba2dd80c7c1e3011b2110
Tong_style <1141632973@qq.com>
2016-07-18 16:58:06 +0800

    the first push

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+function buildCartItems(inputs, allItems) {
+  let cartItems = [];
+
+  for(let value of inputs) {
+    let item = value.split('-');
+    let itemCount = (item[1] || 1);
+    let cartItem = findExistItem(item[0], cartItems);
+
+    if(cartItem) {
+      cartItem.count++ ;
+    }else {
+      let existItem = findExist(item[0], allItems);
+      cartItems.push({item: existItem, count: parseInt(itemCount)});
+    }
+  }
+  return cartItems;
+}
+
+function findExist(item, allItems) {
+  for(let value of allItems) {
+    if(item === value.barcode) {
+      return value;
+    }
+  }
+}
+
+function findExistItem(item, cartItems) {
+  for(let value of cartItems) {
+    if(item === value.item.barcode) {
+      return value;
+    }
+  }
+}
--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -2,28 +2,29 @@
 
 describe('pos', () => {
   let inputs;
+  let allItems = loadAllItems();
 
-  beforeEach(() => {
-    inputs = [
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000003-2',
-      'ITEM000005',
-      'ITEM000005',
-      'ITEM000005'
-    ];
-  });
+beforeEach(() => {
+  inputs = [
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000003-2',
+  'ITEM000005',
+  'ITEM000005',
+  'ITEM000005'
+];
+});
 
-  it('should print correct text', () => {
+it('should print correct text', () => {
 
-    spyOn(console, 'log');
+  spyOn(console, 'log');
 
-    printReceipt(inputs);
+printReceipt(inputs);
 
-    const expectText = `***<没钱赚商店>收据***
+const expectText = `***<没钱赚商店>收据***
 名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
 名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)
 名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
@@ -32,6 +33,45 @@ describe('pos', () => {
 节省：7.50(元)
 **********************`;
 
-    expect(console.log).toHaveBeenCalledWith(expectText);
-  });
+expect(console.log).toHaveBeenCalledWith(expectText);
+});
+
+
+it('cartItems text', () => {
+  let cartItems = buildCartItems(inputs,  allItems);
+
+const expectCartItems = [
+  {
+    item:
+        {
+          barcode:'ITEM000001',
+          name:'雪碧',
+          unit:'瓶',
+          price:3.00
+        },
+        count:5
+  },
+  {
+    item:
+        {
+          barcode:'ITEM000003',
+          name:'荔枝',
+          unit:'斤',
+          price:15.00
+        },
+        count:2
+  },
+  {
+    item:
+        {
+          barcode:'ITEM000005',
+          name:'方便面',
+          unit:'袋',
+          price:4.50
+        },
+        count:3
+  }
+];
+  expect(cartItems).toEqual(expectCartItems);
+});
 });
